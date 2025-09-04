783130b39bf1f61d732227deff60d6d8a197d7a8
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2025-09-05 00:10:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,32 @@ import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  async headers() {
+    return [
+      {
+        // Berlaku untuk semua route
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://socipro.site", // asal yang diizinkan
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   logging: {
     fetches: {
       fullUrl: true,
